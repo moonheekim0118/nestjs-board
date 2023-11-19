5f27e8cdcff0838fdca5f9adf7122c5ff2ce2cdb
moonheekim0118 <hellomooneekim@gmail.com>
2023-11-19 15:48:19 +0900

    feat: board 모듈 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { BoardModule } from './board/board.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [BoardModule],
 })
 export class AppModule {}
